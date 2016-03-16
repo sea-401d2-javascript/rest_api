fa1c2fcbdb82e7c99bba49b747e63af7744351f0
Alemneh <Alemneh@hotmail.com>
2016-03-16 12:29:56 -0700

    added /stats resouce it returns stats on db

--- alemneh/index.js
+++ alemneh/index.js
@@ -11,6 +11,7 @@ app.use(bodyParser());
 
 app.get('/ideas', (req, res) => {
   Idea.find({}, (err, idea) =>{
+    console.log(idea[0]);
     res.json({data: idea});
   });
 });
@@ -21,6 +22,18 @@ app.get('/ideas/:id', (req, res) => {
   });
 });
 
+app.get('/stats', (req, res) => {
+  Idea.find({}, (err, idea) => {
+    var totalIdeas = idea.length;
+    var avrgTeamSize = idea.map((idea) => {
+      return idea.teamSize;
+    }).reduce((a,b) => {return (a + b);})/totalIdeas;
+    res.send('Stats\nTotal Ideas: '+totalIdeas+'\nAvg Teamsize: '+
+              avrgTeamSize);
+  });
+
+});
+
 app.post('/ideas', (req, res) => {
   var newIdea = new Idea(req.body);
   newIdea.save((err, idea) => {
@@ -28,7 +41,20 @@ app.post('/ideas', (req, res) => {
   });
 });
 
+app.put('/ideas/:id', (req, res) => {
+  Idea.findByIdAndUpdate(req.params.id, req.body, (err, idea) =>{
+    if(err) return res.send(err);
+    res.json(idea);
+  });
+});
 
+app.delete('/ideas/:id', (req, res) => {
+  Idea.findById(req.params.id, (err, idea) =>{
+    idea.remove((err, idea) => {
+      res.json({message: 'idea removed'});
+    });
+  });
+});
 
 app.listen(3000, () => {
   console.log('Server running on port 3000');
