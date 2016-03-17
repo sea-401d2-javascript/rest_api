3b866fad46fc8496d5a8d3756faf2dd9a86fadc6
Hugo A Guerra <guerrahugoa@gmail.com>
2016-03-16 21:38:05 -0700

    completed routes

--- hugo-rest-mongo/routes/route-handle.js
+++ hugo-rest-mongo/routes/route-handle.js
@@ -1,4 +1,6 @@
 'use strict';
+let Customer = require(__dirname + '/../models/customers-model');
+let Product = require(__dirname + '/../models/products-model');
 
 module.exports = (middleRouter) => {
   middleRouter.route('/customers')
@@ -25,39 +27,63 @@ module.exports = (middleRouter) => {
   })
   .put((req, res) => {
     console.log('PUT route hit for /customers/:id');
-    Customer.update({id: req.params.id}, req.body, (err, customer) => {
+    Customer.findByIdAndUpdate({id: req.params.id}, req.body, (err, customer) => {
       if (err) return res.send(err);
       res.json(customer);
     });
   })
   .delete((req, res) => {
     console.log('DEL route hit for /customers/:id');
-    Customer.findById
+    Customer.findById(req.params.id, (err, customer) => {
+      customer.remove((err, penguin) => {
+        res.json({message: 'penguin removed'});
+      })
+    })
   });
+
+//product routes
 
   middleRouter.route('/products')
   .get((req, res) => {
     console.log('GET route hit for /products');
+    Product.find({}, (err, products) => {
+      res.json({data: products})
+    });
   })
   .post((req, res) => {
     console.log('POST route hit for /products');
+    newProduct.save((err, product) => {
+      res.json(product);
+    })
   });
   middleRouter.route('/products/:id')
   .get((req, res) => {
     console.log('GET route hit for /products/:id');
+    PRoduct.findById(req.params.id, (err, product) => {
+      res.json(product);
+    })
   })
   .put((req, res) => {
     console.log('PUT route hit for /products/:id');
+    Product.findByIdAndUpdate(req.params.id, req.body, (err, product) => {
+      if (err) return res.send(err);
+      res.json(product);
+    })
   })
   .delete((req, res) => {
     console.log('DEL route hit for /products/:id');
+    Product.findById(req.params.id, (err, product) => {
+      product.remove((err, product) => {
+        res.json({message: 'penguin removed'});
+      })
+    })
   });
 
-  middleRouter.route('/unicorns')
+  middleRouter.route('/isoceles')
   .get((req, res) => {
-    console.log('GET route hit for /unicorns');
+    console.log('GET route hit for /isoceles');
   })
   .post((req, res) => {
-    console.log('POST route hit for /unicorns');
+    console.log('POST route hit for /isoceles');
   })
 }
