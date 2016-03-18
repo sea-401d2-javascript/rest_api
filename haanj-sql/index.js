3528aa121fd114fc2a90890ff7103f6cba39eb24
haanj <joshua.haan@ymail.com>
2016-03-18 09:46:49 -0700

    initial sql refractor commit

---
+++ haanj-sql/index.js
@@ -0,0 +1,132 @@
+'use strict';
+let S_PORT = process.env.S_PORT || require('./.config').S_PORT;
+let DB_PORT = process.env.DB_PORT || require('./.config').DB_PORT;
+
+let bodyParser = require('body-parser');
+let express = require('express');
+let app = express();
+
+let mongoose = require('mongoose');
+let Movie = require('./models/movie_module');
+let Snack = require('./models/snack_module');
+
+mongoose.connect(DB_PORT);
+
+
+app.use(bodyParser.json());
+
+// /Movies routes
+app.route('/movies')
+  .get((req, res) => {
+    console.log('GET request received for /movies');
+    Movie.find({})
+      .populate('actors')
+      .exec((err, movies) => {
+        if (err) return res.send(err);
+        res.json(movies);
+      });
+  })
+  .post((req, res) => {
+    console.log('POST request received for /movies');
+    var newMovie = new Movie(req.body);
+    newMovie.save((err, movie) => {
+      res.json(movie);
+    });
+  });
+
+app.route('/movies/:id')
+  .get((req, res) => {
+    console.log('GET request received for /movies/' + req.params.id);
+    Movie.findById(req.params.id)
+      .populate('actors')
+      .exec((err, movie) => {
+        res.json(movie);
+      });
+  })
+  .put((req, res) => {
+    console.log('PUT request received for /movies/' + req.params.id);
+    Movie.update({_id: req.params.id}, req.body, (err, movie) => {
+      res.json(movie);
+    });
+  })
+  .delete((req, res) => {
+    console.log('DELETE request received for /movies/' + req.params.id);
+    Movie.findById(req.params.id, (err, movie) => {
+      movie.remove();
+      res.json(movie);
+    });
+  });
+
+// /Snacks routes
+app.route('/snacks')
+  .get((req, res) => {
+    console.log('GET request received for /snacks');
+    Snack.find({}, (err, snacks) => {
+      if (err) return res.send(err);
+      res.json(snacks);
+    });
+  })
+  .post((req, res) => {
+    console.log('POST request received for /snacks');
+    var newSnack = new Snack(req.body);
+    newSnack.save((err, snack) => {
+      res.json(snack);
+    });
+  });
+
+app.route('/snacks/:id')
+  .get((req, res) => {
+    console.log('GET request received for /snacks/' + req.params.id);
+    Snack.findById(req.params.id, (err, snack) => {
+      res.json(snack);
+    });
+  })
+  .put((req, res) => {
+    console.log('PUT request received for /snacks/' + req.params.id);
+    Snack.update({_id: req.params.id}, req.body, (err, snack) => {
+      res.json(snack);
+    });
+  })
+  .delete((req, res) => {
+    console.log('DELETE request received for /snacks/' + req.params.id);
+    Snack.findById(req.params.id, (err, snack) => {
+      snack.remove();
+      res.json(snack);
+    });
+  });
+
+// /suggest route
+app.route('/suggest')
+  .get((req, res, next) => {
+    console.log('GET request received for /suggest');
+    res.suggestion = {};
+    next();
+  })
+  .get((req, res, next) => {
+    console.log('Getting random movie');
+    Movie.find({})
+      .populate('actors')
+      .exec((err, movies) => {
+        if (err) return res.send(err);
+        let random = Math.floor(Math.random() * movies.length);
+        res.suggestion.movie = movies[random];
+        next();
+      });
+  })
+  .get((req, res, next) => {
+    console.log('Getting random snack');
+    Snack.find({}, (err, snacks) => {
+      if (err) return res.send(err);
+      let random = Math.floor(Math.random() * snacks.length);
+      res.suggestion.snack = snacks[random];
+      next();
+    });
+  })
+  .get((req, res) => {
+    console.log('Returning random movie and random snack');
+    res.json(res.suggestion);
+  });
+
+app.listen(S_PORT, () => {
+  console.log('Server started on port', S_PORT);
+});
---
+++ haanj-sql/test/http_tests.js
@@ -0,0 +1,144 @@
+'use strict';
+require(__dirname + '/../index');
+
+// var fs = require('fs');
+let chai = require('chai');
+let chaiHTTP = require('chai-http');
+chai.use(chaiHTTP);
+let request = chai.request;
+let expect = chai.expect;
+
+// let DB_PORT = require('../.config').DB_PORT;
+let S_PORT = process.env.S_PORT || require('../.config').S_PORT;
+// let mongoose = require('mongoose');
+let newMovie = {name: 'Paul Blart', imdb: 9.9};
+let newSnack = {name: 'pho', ingredients: ['beef', 'magic']};
+
+describe('/movies resource routing tests', function() {
+  this.timeout(0); // due to latency with mongodb
+
+  it('should send back array of movie objects with GET request', (done) => {
+    request('localhost:' + S_PORT)
+      .get('/movies')
+      .end((err, res) => {
+        expect(err).to.eql(null);
+        expect(res).to.have.status(200);
+        expect(typeof res.body).to.eql('object');
+        expect(res.body.length).to.not.eql(NaN);
+        expect(res.body[0]).to.have.property('_id');
+        done();
+      });
+  });
+
+  it('should create and retrieve new movie document', (done) => {
+    request('localhost:' + S_PORT)
+      .post('/movies')
+      .send(newMovie)
+      .end((err, res) => {
+        expect(err).to.eql(null);
+        expect(res).to.have.status(200);
+        expect(res.body).to.have.property('_id');
+        expect(res.body.name).to.eql(newMovie.name);
+        newMovie._id = res.body._id;
+        request('localhost:' + S_PORT)
+          .get('/movies/'+newMovie._id)
+          .end((err, res) => {
+            expect(err).to.eql(null);
+            expect(res).to.have.status(200);
+            expect(res.body).to.have.property('_id');
+            expect(res.body.name).to.eql(newMovie.name);
+            done();
+          });
+      });
+  });
+
+  it('should delete the test document', (done) => {
+    request('localhost:' + S_PORT)
+      .delete('/movies/' + newMovie._id)
+      .end((err, res) => {
+        expect(err).to.eql(null);
+        expect(res).to.have.status(200);
+        expect(res.body).to.have.property('_id');
+        expect(res.body.name).to.eql(newMovie.name);
+        newMovie._id = res.body._id;
+        request('localhost:' + S_PORT)
+          .get('/movies/' + newMovie._id)
+          .end((err, res) => {
+            expect(res.body).to.eql(null);
+            done();
+          });
+      });
+  });
+});
+
+describe('/snacks resource routing tests', function() {
+  this.timeout(0); // due to latency with mongodb
+
+  it('should send back array of snack objects with GET request', (done) => {
+    request('localhost:' + S_PORT)
+      .get('/snacks')
+      .end((err, res) => {
+        expect(err).to.eql(null);
+        expect(res).to.have.status(200);
+        expect(typeof res.body).to.eql('object');
+        expect(res.body.length).to.not.eql(NaN);
+        expect(res.body[0]).to.have.property('_id');
+        done();
+      });
+  });
+
+  it('should create and retrieve new snack document', (done) => {
+    request('localhost:' + S_PORT)
+      .post('/snacks')
+      .send(newSnack)
+      .end((err, res) => {
+        expect(err).to.eql(null);
+        expect(res).to.have.status(200);
+        expect(res.body).to.have.property('_id');
+        expect(res.body.name).to.eql(newSnack.name);
+        newSnack._id = res.body._id;
+        request('localhost:' + S_PORT)
+          .get('/snacks/'+newSnack._id)
+          .end((err, res) => {
+            expect(err).to.eql(null);
+            expect(res).to.have.status(200);
+            expect(res.body).to.have.property('_id');
+            expect(res.body.name).to.eql(newSnack.name);
+            done();
+          });
+      });
+  });
+
+  it('should delete the test document', (done) => {
+    request('localhost:' + S_PORT)
+      .delete('/snacks/' + newSnack._id)
+      .end((err, res) => {
+        expect(err).to.eql(null);
+        expect(res).to.have.status(200);
+        expect(res.body).to.have.property('_id');
+        expect(res.body.name).to.eql(newSnack.name);
+        newSnack._id = res.body._id;
+        request('localhost:' + S_PORT)
+          .get('/snacks/' + newSnack._id)
+          .end((err, res) => {
+            expect(res.body).to.eql(null);
+            done();
+          });
+      });
+  });
+});
+
+describe('/suggest resource routing tests', function() {
+  this.timeout(0); // due to latency with mongodb
+  it('should test the /suggest route', (done) => {
+    request('localhost:' + S_PORT)
+      .get('/suggest')
+      .end((err,res) => {
+        expect(err).to.eql(null);
+        expect(res).to.have.status(200);
+        expect(res.body).to.have.property('movie');
+        expect(res.body).to.have.property('snack');
+        done();
+      });
+  });
+});
