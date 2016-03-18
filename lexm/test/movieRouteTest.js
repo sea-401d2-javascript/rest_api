ee2235ae4aab863a9624bb5f2c977f7036f4626e
Lex Myers <lex.myers@gmail.com>
2016-03-18 10:43:27 -0700

    halfway through tests

---
+++ lexm/test/movieRouteTest.js
@@ -0,0 +1,112 @@
+'use strict';
+
+process.env.MONGO_URI = 'mongodb://localhost/movie_test';
+var mongoose = require('mongoose');
+var chai = require('chai');
+var chaiHttp = require('chai-http');
+chai.use(chaiHttp);
+
+var request = chai.request;
+var expect = chai.expect;
+
+var Movie = require(__dirname + '/../models').Movie;
+
+require(__dirname + '/../server');
+
+describe('testing Movie API', function() {
+
+  after(function(done) {
+    mongoose.connection.db.dropDatabase(function() {
+      done();
+    });
+  });
+
+  it('should be able to add a new movie', function(done) {
+    request('localhost:3000')
+    .post('/movies')
+    .send({
+      "name": "Sex, Lies, and Videotape",
+      "release_date": "January 20, 1989"
+    })
+    .end(function(err, res) {
+      expect(err).to.eql(null);
+      expect(res.body.name).to.eql('Sex, Lies, and Videotape');
+      expect(Date(res.body.release_date)).to.eql(Date('January 14, 1963'));
+      expect(res.body).to.have.property('_id');
+      done();
+    })
+  });
+
+  it('should be able to retrieve list of movies', function(done) {
+    request('localhost:3000')
+    .get('/movies')
+    .end(function(err, res) {
+      expect(err).to.eql(null);
+      expect(typeof res.body).to.eql('object');
+      expect(Array.isArray(res.body.data)).to.eql(true);
+      done();
+    })
+  });
+
+});
+
+describe('need to have existing movie to test with', function(done) {
+
+  beforeEach(function(done) {
+    var testMovie = new Movie({"name": "Traffic", "release_date": "December 27, 2000"});
+    testMovie.save(function(err, data) {
+      if(err) throw err;
+      this.testMovie = data;
+      done();
+    }.bind(this));
+  });
+
+  it('should have created movie in forEach block', function(done) {
+    expect(this.testMovie.name).to.eql('Traffic');
+    expect(this.testMovie).to.have.property('_id');
+    done();
+  });
+  it('should be able to get info on single movie', function(done) {
+    done();
+  })
+  it('should be able to update movie', function(done) {
+    done();
+  })
+  it('should be able to delete movie', function(done) {
+    done();
+  })
+});
+
+
+
+
+// 'use strict';
+//
+// var chai = require('chai');
+// var chaiHttp = require('chai-http');
+// chai.use(chaiHttp);
+//
+// var request = chai.request;
+// var expect = chai.expect;
+//
+// require(__dirname + '/../server');
+//
+// describe('testing /movies POST route', () => {
+//
+// });
+//
+// describe('testing /movies GET route', () => {
+//
+// });
+//
+// describe('testing /movies:id GET route', () => {
+//
+// });
+//
+// describe('testing /movies:id PUT route', () => {
+//
+// });
+//
+// describe('testing /movies:id DELETE route', () => {
+//
+// });
