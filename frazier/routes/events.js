feedfada364a47161c520b33a464e091ec625f57
Frazier Mork <fraziermork@gmail.com>
2016-03-17 20:24:39 -0700

    got populate working and tested

--- frazier/routes/events.js
+++ frazier/routes/events.js
@@ -2,27 +2,27 @@
 var express = require('express');
 // var mongoose = require('mongoose');
 var Event = require(__dirname + '/../models/events-model.js');
+var Character = require(__dirname + '/../models/characters-model.js');
 // var resources = require(__dirname + '/../lib/resources.js');
 
 var router = express.Router();
 
 router.get('/', (request, response) => {
   console.log('GET request to /events made');
-  Event.find({}, (err, events) => {
+  Event.find({}).populate('charsPresent')
+  .exec( (err, events) => {
     if (err){
-      // response.writeHead(404, {'Content-Type: application/json'});
       return response.status(404).end('404 Not found');
     } else {
-      // response.writeHead(200, {'Content-Type: application/json'});
       return response.status(200).json(events);
     }
   });
 });
 
 router.post('/', (request, response) => {
-  console.log('post to /events made');
+  console.log('POST to /events made');
   console.log('req body is', request.body);
-  if(request.body.name && request.body.date){
+  if(request.body.name){
     var newEvent = new Event(request.body);
     newEvent.save((err, newDBEvent) => {
       console.log('saved');
@@ -31,7 +31,14 @@ router.post('/', (request, response) => {
         response.status(500).end('500 server error');
       } else{
         console.log(newDBEvent.name + ' saved.');
+        console.log(newDBEvent.charsPresent.length);
         response.status(200).json(newDBEvent);
+        if (newDBEvent.charsPresent.length > 0){
+          console.log('Need to add event references to characters');
+          request.body.charsPresent.forEach((currentCharId) => {
+            Character.findOne({_id: currentCharId}).update({$push: {'presentAt': newDBEvent._id}});
+          });
+        }
       }
     });
   } else {
@@ -41,7 +48,9 @@ router.post('/', (request, response) => {
 
 router.get('/:id', (request, response) => {
   console.log('GET request to /events/:id made');
-  Event.findOne({_id: request.params.id}, (err, thatEvent) => {
+  // Event.findOne({_id: request.params.id}, (err, thatEvent) => {
+  Event.find({_id: request.params.id}).populate('charsPresent')
+  .exec((err, thatEvent) => {
     if (err){
       console.log('Error finding requested event, error was ', err);
       response.status(400).end();
@@ -49,18 +58,25 @@ router.get('/:id', (request, response) => {
       response.status(200).json(thatEvent);
     }
   });  
-    
 });
 
 router.put('/:id', (request, response) => {
   console.log('PUT request to /event/:id made');
-  Event.findOneAndUpdate({_id: request.params.id}, request.body, {new: true}, (err, DBEvent) => {
+  Event.findOneAndUpdate({_id: request.params.id}, request.body, {new: true}, (err, newDBEvent) => {
     if(err){
       console.log('Error updating requested event, error was ', err);
       response.status(400).end();
     } else {
-      console.log('Updated ' + DBEvent.name);
-      response.status(200).json(DBEvent);
+      console.log('Updated ' + newDBEvent.name);
+      response.status(200).json(newDBEvent);
+      newDBEvent.charsPresent.forEach((currentCharId) => {
+        Character.findOne({_id: currentCharId}, (err, thisCharacter) => {
+          if (thisCharacter.presentAt.indexOf(newDBEvent._id) === -1){
+            console.log('Need to add event reference to character ' + thisCharacter.name);
+            Character.findOneAndUpdate({_id: currentCharId}, {$push: {'presentAt': newDBEvent._id}});
+          }
+        });
+      });
     }
   });
 });
